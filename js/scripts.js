3cc8f818c73111492e5e8c1af3385fd6e0813030
divman70 <dkoch@eagles.ewu.edu>
2015-03-04 00:33:28 -0800

    added input & button

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,45 @@
-  var cheney = '99004';
-
-  $.simpleWeather
-  	location: spokane,
+  $.simpleWeather ({
+  	location: 99004,
   	unit: 'f',
 
+// Get _weather_ object
 success: function(weather){
+// Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
+      // get and store state
+      var state = weather.region;
+      // get and store current weather
+      var currentTemp = weather.currently;
 
+      
+      // Output to hooks in HTML
+      $('.temp').text(temp);
+      $('.city').text(city);
+      $('.state').text(state);
+      $('.currently').text(currentTemp);
+      
+      
 },
 
-error: function(message){}
-      }, options);
+error: function(message){
+	$('body').html('<p>' + error + '</p>');
+}
+      });
+
+
+// show zip
+var showZip = function () {
+	var zip =  document.getElementById('val').value;
+    $('.zip').text(zip);
+    
+};
+
+// Click button, showName
+$('button').click(function(){
+  
+  //Call showName
+  showZip();
+  
+});
