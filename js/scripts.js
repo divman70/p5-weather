c56034381a881dfb57d93ba79da3775d6ffdeaba
divman70 <dkoch@eagles.ewu.edu>
2015-03-14 22:35:23 -0700

    added all conditions & basic styles

--- js/scripts.js
+++ js/scripts.js
@@ -23,14 +23,14 @@ success: function(weather){
       // output weather code
       console.log(type);
       // get and store weather for next day
-      var upcomingWeather = weather.forecast[1].day + ' ' + weather.forecast[1].high;
+      // var upcomingWeather = weather.forecast[1].day + ' ' + weather.forecast[1].high;
       
       // Output to hooks in HTML
       $('.temp').text(temp);
       $('.city').text(city);
       $('.state').text(state);
       $('.currently').text(currentTemp);
-      $('p').text(upcomingWeather); 
+      // $('p').text(upcomingWeather); 
 
       // checks for dangerous weather conditions 
       if (type == 0 || type == 1 || type == 2 || type == 3 || type == 4) {
@@ -42,7 +42,7 @@ success: function(weather){
           $('body').css('background-image', 'url("img/rain.jpg")');
         }
         // checks for snowy weather conditions
-        else if (type == 13 || type == 14 || type == 15 || type == 16 || type == 33 ||  type == 41 || type == 43 || type == 46) {
+        else if (type == 13 || type == 14 || type == 15 || type == 16 || type == 33 ||  type == 41 || type == 43 || type == 46 || temp <= 36) {
           $('body').css('background-image', 'url("img/ice.jpg")');
         }
         // checks for sunny weather conditions
@@ -66,60 +66,60 @@ error: function(message){
 
 });
 
-if (navigator.geolocation) {
-    // Yes! Show button
-    $('.getGeolocation').show(); 
-  } else {
-    // No. Hide button
-    $('.getGeolocation').hide();
-  }
+// if (navigator.geolocation) {
+//     // Yes! Show button
+//     $('.getGeolocation').show(); 
+//   } else {
+//     // No. Hide button
+//     $('.getGeolocation').hide();
+//   }
 
 
-  $('.getGeolocation').on('click', function() {
+//   $('.getGeolocation').on('click', function() {
   
-    navigator.geolocation.getCurrentPosition(function(position) {
-    //load weather using your lat/lng coordinates. See _loadWeather()_ below
-    loadWeather(position.coords.latitude+','+position.coords.longitude); 
-    // See latitute & longitude. Note, wait a few seconds
-    console.log(position.coords.latitude+','+position.coords.longitude);
-  });
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//     //load weather using your lat/lng coordinates. See _loadWeather()_ below
+//     loadWeather(position.coords.latitude+','+position.coords.longitude); 
+//     // See latitute & longitude. Note, wait a few seconds
+//     console.log(position.coords.latitude+','+position.coords.longitude);
+//   });
   
-});
+// });
 
-  var loadWeather = function(location) {
+//   var loadWeather = function(location) {
     
-    $.simpleWeather({
-    location: location,
+//     $.simpleWeather({
+//     location: location,
     
-    // Get _weather_ object
-    success: function(weather) {
+//     // Get _weather_ object
+//     success: function(weather) {
         
-      console.log(weather);
+//       console.log(weather);
       
-      // Get & store temperature
-      var temp = weather.temp;
-      // Get & store city
-      var city = weather.city;
+//       // Get & store temperature
+//       var temp = weather.temp;
+//       // Get & store city
+//       var city = weather.city;
 
-      var state = weather.region;
+//       var state = weather.region;
 
-      var currentTemp = weather.currently;
+//       var currentTemp = weather.currently;
 
             
-      // Output to hooks in HTML
-      $('.temp').text(temp);
-      $('.city').text(city);
-      $('.state').text(state);
-      $('.currently').text(currentTemp);
+//       // Output to hooks in HTML
+//       $('.temp').text(temp);
+//       $('.city').text(city);
+//       $('.state').text(state);
+//       $('.currently').text(currentTemp);
       
       
-      // See console for _weather_ object
-      console.log(weather);
-    }
+//       // See console for _weather_ object
+//       console.log(weather);
+//     }
   
-  });
+//   });
     
-}; // end of _loadWeather()_ function
+// }; // end of _loadWeather()_ function
 
 
   
